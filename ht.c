e5f7a39da37578609b28c723f55bfda559453c34
Ben Hoyt <benhoyt@gmail.com>
2021-03-26 00:31:16 +1300

    Another comment tweak

--- ht.c
+++ ht.c
@@ -56,7 +56,7 @@ void ht_destroy(ht* table) {
 #define FNV_OFFSET 14695981039346656037UL
 #define FNV_PRIME 1099511628211UL
 
-// Return 64-bit FNV-1a hash for key (NUL-terminated). See description at:
+// Return 64-bit FNV-1a hash for key (NUL-terminated). See description:
 // https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
 static uint64_t hash_key(const char* key) {
     uint64_t hash = FNV_OFFSET;
